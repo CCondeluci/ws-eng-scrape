16f35623d06625f107627b2d5b2d4f3114ebb79b
Carmen Condeluci <crc73@pitt.edu>
2019-01-24 15:09:07 -0500

    Update bushi-scraper.js

--- bushi-scraper.js
+++ bushi-scraper.js
@@ -8,7 +8,53 @@ module.exports = (card_url) => {
         if (!error) {
             let full_html = html_parser.parse(body);
             let cardDetail = full_html.querySelector("#cardDetail");
-            console.log(cardDetail.structuredText);
+            let detailRows = cardDetail.querySelectorAll('tr');
+            let card = {};
+            for (let detailRow of detailRows) {
+                let dataRows = detailRow.querySelectorAll('td');
+                let headerRows = detailRow.querySelectorAll('th');
+                for (let i = 0; i < dataRows.length; i++) {
+                    if (headerRows[i] !== undefined) {
+                        let currHeader = headerRows[i].structuredText;
+                        let currData = dataRows[i].structuredText;
+                        let currDataRaw = dataRows[i];
+                        if (currHeader == "Color" || currHeader == "Side") {
+                            card[currHeader] = currDataRaw.innerHTML.match(/(?<=\/partimages\/)(.*?)(?=\s*\.gif)/gi)[0].toUpperCase();
+                        } 
+                        else if (currHeader == "Trigger") {
+                            let triggerCheck = currDataRaw.innerHTML.match(/(?<=\/partimages\/)(.*?)(?=\s*\.gif)/gi);
+                            if (triggerCheck.length > 1) {
+                                card[currHeader] = 'TWO SOUL';
+                            } else {
+                                card[currHeader] = triggerCheck[0].toUpperCase();
+                            }
+                        }
+                        else if (currHeader == "Soul") {
+                            let soulArr = currDataRaw.querySelectorAll('img');
+                            card[currHeader] = soulArr.length;
+                        }
+                        else if (currHeader == "Special Attribute") {
+                            card['Attributes'] = currData.split(' ・ ');
+                        }
+                        else if (currHeader == "Text") {
+                            card[currHeader] = currData.split('\n');
+                        }
+                        else if (currHeader == "Card No.") {
+                            card['Code'] = currData;
+                        }
+                        else if (currHeader == "Card Type") {
+                            card['Type'] = currData;
+                        }
+                        else if (currHeader == "Card Name") {
+                            // do not push to object
+                        }
+                        else {
+                            card[currHeader] = currData;
+                        }
+                    }
+                }
+            }
+            console.log(card);
         } else {
             console.log(error);
         }
